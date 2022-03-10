3fd7486947586aa4f50cd6b4dff41fcac7666492
Rohit <95957671+Rohitb1107@users.noreply.github.com>
2022-03-10 17:59:29 +0530

    styling

--- Drum Kit/script.js
+++ Drum Kit/script.js
@@ -1,9 +1,10 @@
-document.addEventListener("keydown", function (e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    if (!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing');
-    key.classList.add('playing');
-    key.classList.add('playing');
-});
+// window.addEventListener("keydown", function (e) {
+//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//     const key = document.querySelector(`key[data-key="${e.keyCode}"]`);
+//     if (!audio) return;
+//     audio.currentTime = 0;
+//     audio.play();
+//     key.classList.add('playing');
+//     // key.classList.remove('playing');
+//     // key.classList.toggle('playing');
+// });
