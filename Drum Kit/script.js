76cd07993f95f0a255950e1387126c5ba3ef8fec
Rohit <95957671+Rohitb1107@users.noreply.github.com>
2022-03-10 17:02:38 +0530

    css add

---
+++ Drum Kit/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("keydown", function (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+});
